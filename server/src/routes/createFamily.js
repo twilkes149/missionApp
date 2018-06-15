f3f6e925b0385284573d57536d054a3411e402a0
Tucker Wilkes <twilkes149@gmail.com>
2018-06-14 20:52:27 -0500

    added create family route

--- server/src/routes/createFamily.js
+++ server/src/routes/createFamily.js
@@ -8,8 +8,32 @@ router.post('/createFamily', async (req, res, next) => {
   console.log("create family");
   let conn = res.locals.conn;
   let email = res.locals.email;
+  let familyName = req.body.familyName ? Database.sanitize(req.body.familyName, conn) : null;
 
-  res.status(200).send({success: true, message: "families"});
+  if (!familyName) {
+    let error = new Error("Family name not provided");
+    error.status = 400;
+    error.body = {success: false, message: 'Family name not provided'};
+    return next(error);
+  }  
+
+  try {
+    let query = `INSERT INTO families (name) VALUES ("${familyName}")`;
+    await conn.query(query);
+
+    query = `SELECT MAX(\`key\`) as \'key\' FROM families WHERE name = "${familyName}"`;
+    let result = await conn.query(query);
+    
+    let key = result[0].key;
+    res.status(200).send({success: true, message: 'Created family', familyKey: key});
+  }
+  catch (error) {
+    error = new Error('SQL error');
+    error.status = 500;
+    error.body = {success: false, message: "SQL Error"};
+    return next(error);
+  }
+  conn.end();
 });
 
 module.exports = router;
