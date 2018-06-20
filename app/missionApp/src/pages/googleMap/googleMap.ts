ffbd5eb729da3dfb36c03fb14b78dc3540f14a5a
Tucker Wilkes <twilkes149@gmail.com>
2018-06-19 23:31:29 -0500

    added get families to api

--- app/missionApp/src/pages/googleMap/googleMap.ts
+++ app/missionApp/src/pages/googleMap/googleMap.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import {
   GoogleMaps,
   GoogleMap,
@@ -10,17 +10,51 @@ import {
   Marker
 } from '@ionic-native/google-maps';
 
+//providers
+import { ApiProvider } from '../../providers/api/api'; 
+
 @Component({
   selector: 'page-googleMap',
-  templateUrl: 'googleMap.html'
+  templateUrl: 'googleMap.html',
+  providers: [ApiProvider]
 })
 export class GoogleMapPage {  
   map: GoogleMap;
-  constructor(public navCtrl: NavController) {
+  private familyKeys;
+
+  constructor(public navCtrl: NavController, 
+    private api: ApiProvider,
+    public alert: AlertController) {
+    this.familyKeys = null;
   }
 
-  ionViewDidLoad() {
+  //when the page is about to load
+  async ionViewWillEnter() {
+    //get list of persons for each family this user belongs to
+    this.familyKeys = await this.api.getFamilyKeys()
+    .catch((error) => {
+      const message = this.alert.create({
+        title: 'Error',
+        subTitle: error.message,
+        buttons: ['OK']
+      });
+      message.present();
+    });    
+  }
+
+  ionViewDidLoad() {    
     this.loadMap();
+    if (!this.familyKeys || !this.familyKeys[0]) {
+      const message = this.alert.create({
+        title: 'Error',
+        subTitle: 'You have not join any families yet. Please go to the settings page, to join or create a family',
+        buttons: ['OK']
+      });
+      message.present();
+    }
+    else {
+      this.addMarkers();
+    }
   }
 
   loadMap() {
@@ -31,13 +65,15 @@ export class GoogleMapPage {
            lat: 43.0741904,
            lng: -89.3809802
          },
-         zoom: 18,
+         zoom: 10,
          tilt: 0
        }
     };
 
     this.map = GoogleMaps.create('mapCanvas', mapOptions);
+  }
 
+  addMarkers() {
     let marker: Marker = this.map.addMarkerSync({
       title: 'Ionic',
       icon: 'blue',
--- app/missionApp/src/providers/api/api.ts
+++ app/missionApp/src/providers/api/api.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HTTP } from '@ionic-native/http';
+import {Storage} from '@ionic/storage';
 
 /*
   Generated class for the ApiProvider provider.
@@ -9,19 +10,50 @@ import { HTTP } from '@ionic-native/http';
 */
 @Injectable()
 export class ApiProvider {
-  private baseUrl = 'https://twilkes-base-server.herokuapp.com/';
-  private authToken;  
+  //private baseUrl = 'https://twilkes-base-server.herokuapp.com/';
+  private baseUrl = 'http://192.168.1.140:8080/';
+  static authToken;  
+  static familyKeys;
 
   constructor(public http: HTTP) {
     console.log('Hello ApiProvider Provider');
-    this.authToken = null;
+    ApiProvider.authToken = null;
+    ApiProvider.familyKeys = null;
+  }
+
+  //gets a list of all the families this user belongs to
+  //object returned has structure: {name: '', familyKey: }
+  getFamilyKeys() {
+    this.http.setDataSerializer('json');    
+    if (ApiProvider.familyKeys) {
+      return Promise.resolve(ApiProvider.familyKeys);
+    }
+    else {
+      return new Promise((resolve, reject) => {
+        let body = {
+          authToken: ''+ApiProvider.authToken,
+        };
+        this.http.get(this.baseUrl + 'family', body, {})
+        .then((response) => {
+          ApiProvider.familyKeys = JSON.parse(response.data);
+          resolve(ApiProvider.familyKeys);
+        })
+        .catch((error) => {
+          console.log(error);
+          if (error.error)      
+            reject(JSON.parse(error.error));
+          else//not sure what happened
+            reject({message: "Unknown error occured"});
+        });
+      });
+    }
   }
 
   //calls api for login and returns promise
   login(email, password) {
     this.http.setDataSerializer('json');
-    if (this.authToken) {
-      return Promise.resolve(this.authToken);
+    if (ApiProvider.authToken) {
+      return Promise.resolve(ApiProvider.authToken);
     }
     else {
       let body = {
@@ -32,8 +64,8 @@ export class ApiProvider {
       return new Promise((resolve, reject) => {
         this.http.post(this.baseUrl + 'login', body, {})
         .then((response) => {          
-          this.authToken = JSON.parse(response.data).token;//save the auth token
-          resolve(this.authToken);
+          ApiProvider.authToken = JSON.parse(response.data).token;//save the auth token          
+          resolve(ApiProvider.authToken);
         })
         .catch((error) => {//let user know something happened, probalby not caused by my api
           if (error.error)      
@@ -48,8 +80,8 @@ export class ApiProvider {
   //calls api for register and returns a promise
   register(password, email, firstName, lastName) {
     this.http.setDataSerializer('json');
-    if (this.authToken) {
-      return Promise.resolve(this.authToken);
+    if (ApiProvider.authToken) {
+      return Promise.resolve(ApiProvider.authToken);
     }
     else {
       return new Promise((resolve, reject) => {
@@ -62,8 +94,8 @@ export class ApiProvider {
 
         this.http.post(this.baseUrl + 'register', body, {})
         .then((response) => {
-          this.authToken = JSON.parse(response.data).token;//save the auth token
-          resolve(this.authToken);
+          ApiProvider.authToken = JSON.parse(response.data).token;//save the auth token         
+          resolve(ApiProvider.authToken);
         })
         .catch((error) => {
           if (error.error)
@@ -119,6 +151,6 @@ export class ApiProvider {
 
   //destroys current user data
   logout() {
-    this.authToken = null;
+    ApiProvider.authToken = null;
   }
 }
